226dc0ce6cd171231d43cbadf5030e4c82bdf0d1
bunleaps <bunleap.sorn@gmail.com>
2023-02-15 13:17:40 +0700

    mongodb integration

--- controllers/users.js
+++ controllers/users.js
@@ -1,43 +1,38 @@
+import User from "../models/user.js";
 import { v4 as uuid } from "uuid";
 
-let users = [];
-
-export const getUsers = (req, res) => {
+export const getUsers = async (req, res) => {
+  const users = await User.find({});
   res.json(users);
 };
 
-export const createUser = (req, res) => {
-  const user = {
+export const createUser = async (req, res) => {
+  const user = new User({
     id: uuid(),
     username: req.body.username,
     age: req.body.age,
-  };
-  users.push(user);
+  });
+  await user.save();
   console.log(`User: ${user.username} created`);
   res.json(user);
 };
 
-export const getUser = (req, res) => {
-  const user = users.find((user) => user.id === req.params.id);
+export const getUser = async (req, res) => {
+  const user = await User.findOne({ id: req.params.id });
   res.json(user);
 };
 
-export const deleteUser = (req, res) => {
-  users = users.filter((user) => user.id !== req.params.id);
+export const deleteUser = async (req, res) => {
+  await User.deleteOne({ id: req.params.id });
   console.log(`User: ${req.params.id} deleted`);
   res.json({ msg: `ID: ${req.params.id} deleted` });
 };
 
-export const updateUser = (req, res) => {
-  const user = users.find((user) => user.id === req.params.id);
+export const updateUser = async (req, res) => {
+  await User.findOneAndUpdate(
+    { id: req.params.id },
+    { username: req.body.username, age: req.body.age }
+  );
 
-  if (user) {
-    user.username = req.body.username;
-    user.age = req.body.age;
-
-    console.log(`User: ${req.body.username} updated`);
-    res.json({ msg: `User: ${req.body.username} updated` });
-  } else {
-    res.json({ msg: `User does not exist in the database!` });
-  }
+  res.json({ msg: `User: ${req.body.username} updated` });
 };
--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 import express from "express";
 import bodyParser from "body-parser";
 import router from "./routes/users.js";
+import mongoose from "mongoose";
 
 const app = express();
 const port = 3000;
+const DATABASE_URL =
+  "mongodb+srv://learndbnodejs:learndbnodejs@cluster0.zu2p3.mongodb.net/?retryWrites=true&w=majority";
 
+// MongoDB || Mongoose --------------------------------
+mongoose.set("strictQuery", true);
+mongoose.connect(DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const database = mongoose.connection;
+database.on("error", console.error.bind(console, "Connection Error"));
+database.once("open", function () {
+  console.log("Database Connected");
+});
+
+// Express App Routes --------------------------------
 app.use(bodyParser.json());
-
 app.use("/user", router);
-
 app.get("/", (req, res) => res.send("Welcome to the Users API!"));
-
 app.all("*", (req, res) =>
   res.send("You've tried reaching a route that doesn't exist.")
 );
 
+// Server Start --------------------------------
 app.listen(port, () => console.log(`Listening on port ${port}`));
